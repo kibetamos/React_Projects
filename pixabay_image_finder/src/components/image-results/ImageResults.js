c12f93f47a257d1b90a03d805ad8d04de2244d78
Kibet Amos <amos.rotich@strathmore.edu>
2024-07-10 16:35:40 +0300

    add search

--- pixabay_image_finder/src/components/image-results/ImageResults.js
+++ pixabay_image_finder/src/components/image-results/ImageResults.js
@@ -1,9 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import { GridList, GridTile } from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import ActionZoomIn from 'material-ui/svg-icons/action/zoom-in';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+
 
 export default class ImageResults extends Component {
+
+
   render() {
+    let imageListContent;
+    const {images} = this.props;
+    if(images){
+        imageListContent = (
+            <GridList  cols={3} >
+                    {images.map(img =>(
+                        <GridTile
+                        title={img.tags}
+                        key={img.id}
+                        subtitle={
+                            <span>
+                                by<strong>{img.user}</strong>
+                            </span>
+                        }
+                        actionIcon={
+                            <IconButton>
+                                <ZoomIn color="white"/>
+                            </IconButton>
+                        }
+                        
+                        >
+                            <img src={img.largeImageURL} alt=''/>
+                        </GridTile>
+                    ))}
+            </GridList>
+        )
+
+    }else{
+        imageListContent = null
+    }
     return (
-      <div>ImageResults</div>
-    )
-  }
+      <div>
+        {imageListContent}
+        </div>
+    );
 }
+  }
+
+  ImageResults.propTypes = {
+    images: propTypes.array.isRequired
+  };
